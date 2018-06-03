c7fcc6890c74d470e2c0942768dc9f3ab639fdbd
Maxime3399 <maxime3399.minecraft@gmail.com>
2018-06-03 11:33:01 +0200

    Profile command
    Ajout de la commande permettant de consulter son profile et de consulter le profile des autres joueurs.

---
+++ src/fr/Maxime3399/Maz/commands/ProfileCMD.java
@@ -0,0 +1,63 @@
+package fr.Maxime3399.Maz.commands;
+
+import fr.Maxime3399.Maz.utils.MySQLUtils;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.User;
+
+public class ProfileCMD {
+	
+	public static void execute(Message message, String command) {
+		
+		String args[] = command.split(" ");
+		
+		if(args.length == 1) {
+			
+			String id = message.getAuthor().getId();
+			int exp = MySQLUtils.getInt("maz_players", "id", message.getAuthor().getId(), "exp");
+			int level = MySQLUtils.getInt("maz_players", "id", message.getAuthor().getId(), "level");
+			int money = MySQLUtils.getInt("maz_players", "id", message.getAuthor().getId(), "exp");
+			String birthday = MySQLUtils.getString("maz_players", "id", message.getAuthor().getId(), "birthday");
+			
+			if(birthday.equals("none")) {
+				birthday = "*Non définis*";
+			}else {
+				birthday = "`"+birthday+"`";
+			}
+			
+			message.getChannel().sendMessage(" __Profile de "+message.getAuthor().getAsMention()+"__\n \n**Informations :**\nNom : `"+message.getAuthor().getName()+"`\nID : `"+id+"`\nAnniversaire : "+birthday+"\n \n**Compte :**\nMoney : `"+money+"`\nXP : `"+exp+"/?`\nNiveau : `"+level+"`").complete();
+			
+		}else if(args.length == 2) {
+			
+			if(message.getMentionedMembers().get(0) == null) {
+				
+				message.getChannel().sendMessage(message.getAuthor().getAsMention()+" | Commande invalide ! Aide : `//profile {@user}`").complete();
+				
+			}else {
+				
+				User user = message.getMentionedMembers().get(0).getUser();
+				
+				String id = user.getId();
+				int exp = MySQLUtils.getInt("maz_players", "id", user.getId(), "exp");
+				int level = MySQLUtils.getInt("maz_players", "id", user.getId(), "level");
+				int money = MySQLUtils.getInt("maz_players", "id", user.getId(), "exp");
+				String birthday = MySQLUtils.getString("maz_players", "id", user.getId(), "birthday");
+				
+				if(birthday.equals("none")) {
+					birthday = "*Non définis*";
+				}else {
+					birthday = "`"+birthday+"`";
+				}
+				
+				message.getChannel().sendMessage(" __Profile de "+user.getAsMention()+"__\n \n**Informations :**\nNom : `"+user.getName()+"`\nID : `"+id+"`\nAnniversaire : "+birthday+"\n \n**Compte :**\nMoney : `"+money+"`\nXP : `"+exp+"/?`\nNiveau : `"+level+"`").complete();
+				
+			}
+			
+		}else {
+			
+			message.getChannel().sendMessage(message.getAuthor().getAsMention()+" | Commande invalide ! Aide : `//profile {@user}`").complete();
+			
+		}
+		
+	}
+
+}
--- src/fr/Maxime3399/Maz/managers/CommandsManager.java
+++ src/fr/Maxime3399/Maz/managers/CommandsManager.java
@@ -1,5 +1,6 @@
 package fr.Maxime3399.Maz.managers;
 
+import fr.Maxime3399.Maz.commands.ProfileCMD;
 import fr.Maxime3399.Maz.commands.RegisterCMD;
 import fr.Maxime3399.Maz.commands.StopCMD;
 import net.dv8tion.jda.core.entities.Message;
@@ -15,6 +16,10 @@ public class CommandsManager {
 		}else if(command.startsWith("register")) {
 			
 			RegisterCMD.execute(message, command);
+			
+		}else if(command.startsWith("profile")) {
+			
+			ProfileCMD.execute(message, command);
 			
 		}
 		
